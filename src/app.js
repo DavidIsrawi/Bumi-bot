8af2d075f1fd0336007956007c2d131c27094c4e
David <davidisrawi@gmail.com>
2021-02-01 17:47:54 -0800

    connection to twitch + auth

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const tmi = require('tmi.js');
+const env = require('../config.json');
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: env.USERNAME,
+		password: env.OAUTH_TOKEN
+	},
+	channels: [ env.MY_CHANNEL ]
+});
+
+client.connect().catch(console.error);
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
